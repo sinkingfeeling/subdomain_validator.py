873509e8060ac20e2f1cf832a2b83ab551e70c55
skarz <40373461+sinkingfeeling@users.noreply.github.com>
2023-04-05 18:44:24 -0700

    Update validator.ps1

--- validator.ps1
+++ validator.ps1
@@ -1,12 +1,22 @@
-$domain = "domain.com"
+$domain = "whiskergalaxy.com"
 $start = 1
 $end = 400
 $outputFile = "valid_results.txt"
+$jobs = @()
 
 for ($i = $start; $i -le $end; $i++) {
-    $hostname = "us-{0:D3}.{1}" -f $i, $domain
-    $result = nslookup $hostname
-    if ($result -match "Name:") {
-        Add-Content -Path $outputFile -Value "$hostname: $result"
-    }
+    $hostname = "bne-{0:D3}.{1}" -f $i, $domain
+    Write-Progress -Activity "Testing domains" -Status "Testing $hostname" -PercentComplete (($i / $end) * 100)
+    $jobs += Start-Job -ScriptBlock {
+        param ($hostname)
+        $result = nslookup $hostname -timeout 500
+        if ($result -match "Name:") {
+            $domain = $result -replace '\n', ' ' -replace '\r', '' -replace '.*: (\S+)\..*', '$1'
+            return $domain
+        }
+    } -ArgumentList $hostname
 }
+
+$results = $jobs | Receive-Job -Wait -AutoRemoveJob | Sort-Object | Get-Unique
+Add-Content -Path $outputFile -Value $results
+Write-Progress -Activity "Testing domains" -Status "Completed" -PercentComplete 100
